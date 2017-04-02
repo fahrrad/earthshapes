971625f2e71b151ec250146bde283a5c84dc7290
Ward Coessens <ward.coessens@gmail.com>
2017-04-02 11:16:57 +0200

    Add background plane (no texture yet)

--- scene.js
+++ scene.js
@@ -6,9 +6,17 @@ var renderer, scene, camera, cube, pot, wireframe;
 function init() {
 		scene = new THREE.Scene();
 
+		// background
+		var backgroundPlaneGeo = new THREE.PlaneGeometry(200,200);
+		var backgroundMaterial = new THREE.MeshBasicMaterial({color: 0xffffff });
+		var backgroundPlane = new THREE.Mesh(backgroundPlaneGeo,
+																				 backgroundMaterial); 
+		backgroundPlane.position.z = -30;
+		scene.add(backgroundPlane);
+		
 		// Pot
 		var material =
-				new THREE.LineBasicMaterial( { color: 0xffffff, linewidth: 2 } );
+				new THREE.LineBasicMaterial( { color: 0x000000, linewidth: 2 } );
 		var loader = new THREE.STLLoader();
 		loader.load(
 				'models/ellipse.stl',
@@ -16,12 +24,14 @@ function init() {
 						pot = new THREE.EdgesGeometry( geometry );
 						wireframe = new THREE.LineSegments( pot, material );
 						wireframe.rotation.x = -1;
+						wireframe.position.y = 2;
+						wireframe.position.x = -20;
 						scene.add( wireframe );
 				}
 		);
 
 
-		camera = new THREE.PerspectiveCamera(75,
+		camera = new THREE.PerspectiveCamera(50,
 																				 window.innerWidth/window.innerHeight, 0.1, 1000);
 		renderer = new THREE.WebGLRenderer( { antialias: true } );
 
@@ -29,10 +39,10 @@ function init() {
 		document.body.appendChild(renderer.domElement);
 
 		camera.position.z = 40;
-		camera.lookAt( scene.position );
-		renderer.render(scene, camera);
-
-}
+		camera.position.y += 10;
+		//camera.lookAt( scene.position );
+							renderer.render(scene, camera);}
+		;
 
 function render() {
 		requestAnimationFrame( render );
