3d300a9e9cf9c1b294bd8b6f9d05d702a92e08db
Ward Coessens <ward.coessens@gmail.com>
2017-04-02 11:02:18 +0200

    cleanup

--- scene.js
+++ scene.js
@@ -6,28 +6,21 @@ var renderer, scene, camera, cube, pot, wireframe;
 function init() {
 		scene = new THREE.Scene();
 
+		// Pot
 		var material =
 				new THREE.LineBasicMaterial( { color: 0xffffff, linewidth: 2 } );
-
 		var loader = new THREE.STLLoader();
 		loader.load(
-				// resource URL
 				'models/ellipse.stl',
-				// Function when resource is loaded
 				function ( geometry ) {
 						pot = new THREE.EdgesGeometry( geometry );
 						wireframe = new THREE.LineSegments( pot, material );
 						wireframe.rotation.x = -1;
 						scene.add( wireframe );
-				},
-				// Function called when download progresses
-				function ( xhr ) {
-						console.log( (xhr.loaded / xhr.total * 100) + '% loaded' );
 				}
 		);
 
 
-		// Lights
 		camera = new THREE.PerspectiveCamera(75,
 																				 window.innerWidth/window.innerHeight, 0.1, 1000);
 		renderer = new THREE.WebGLRenderer( { antialias: true } );
@@ -46,8 +39,6 @@ function render() {
 		renderer.render( scene, camera );
 		if(wireframe){
 				wireframe.rotation.z += 0.01;
-				
 		}
-		
 }
 render();
