c58a29166016d9c1a33c95bd5489f7d8b90e641a
Ward Coessens <ward.coessens@gmail.com>
2017-07-31 20:54:48 +0200

    Pot zonder hoek, in vol materiaal

--- models/square.scad
+++ models/square.scad
@@ -4,7 +4,8 @@
      linear_extrude(height = height, center = true, convexity = 10,
 scale=angle) {
         translate([0, 0, 0]){
-            square(size = 4, center=true);
+            square(size = width, center=true
+);
             }
         }
     }
@@ -13,11 +14,13 @@ scale=angle) {
 module hollow_earthsphere(width, height, angle) {
     difference(width, height, angle) {
         earthsphere(width, height, angle);
-        translate([0,0,1])
+        translate([0,0,.2])
+        scale([0.9,0.9,1])
         earthsphere(width, height, angle);
     }
 }
 
-translate([0,0,3]){
-hollow_earthsphere(2,6,2);
+translate([0,0,4]){
+hollow_earthsphere(8,8,1
+   );
 }
--- scene.js
+++ scene.js
@@ -2,36 +2,54 @@ function renderPot(parentElement){
 		var width = 470;
 		var height = 400;
 
-		var renderer, scene, camera, cube, wireframe,
-				material, rotateP;
+		var renderer, scene, camera, cube, pot,
+				rotateP;
 
 		rotateP = true;
 		
 		var loader = new THREE.STLLoader();
-		
+		var materialLoader = new THREE.TextureLoader();
+			
 		function loadModel(modelFile){
 				loader.load(
 						modelFile,
 						function ( geometry ) {
-								var pot = new THREE.EdgesGeometry( geometry );
-								scene.remove(wireframe);
-								wireframe = new THREE.LineSegments( pot, material );
-								wireframe.rotation.x = -1.5;
-								wireframe.position.y = -4;
-								wireframe.position.x = -6;
-								scene.add( wireframe );
+								loadModelWithTexture(geometry, 'assets/wooden.jpg');
 						}
 				);
 		}
+
+		function loadModelWithTexture(geometry, texture) {
+				materialLoader.load(texture, function(m){
+						var purpleMat = new THREE.MeshPhongMaterial( { color: 0xe8ba23, shininess: 30 } );
+						var material = new THREE.MeshBasicMaterial( { map:m } );
+						scene.remove(pot);
+						pot = new THREE.Mesh( geometry, purpleMat );
+						
+						pot.rotation.x = -1.5;
+
+						pot.position.y = -10;
+						pot.position.x = -5;
+
+						pot.castShadow = true;
+						pot.receiveShadow = true;
+						scene.add(pot);
+							
+				},function ( xhr ) {
+						console.log( (xhr.loaded / xhr.total * 100) + '% loaded' );
+				},function ( xhr ) {
+						console.log( 'An error happened' );
+				});
+		}
 		
 		function setHeight(x){
-				wireframe.scale.z = x;
+				pot.scale.z = x;
 		}
 		function setLength(x){
-				wireframe.scale.y = x;
+				pot.scale.y = x;
 		}
 		function setWidth(x){
-				wireframe.scale.x = x;
+				pot.scale.x = x;
 		}
 		function setAngle(x){
 				console.log('set angle not supported');}
@@ -71,36 +89,58 @@ function renderPot(parentElement){
 				var sprite = new THREE.Sprite( spriteMaterial );
 				sprite.scale.x = 25;
 				sprite.scale.y = 50;
-				sprite.position.z -= 15;
-				sprite.position.y = 10;
+				sprite.position.z -= 25;
+				sprite.position.y = 0;
 				sprite.position.x = 17;
 				scene.add( sprite );
-								
+
+				//Lights
+				hemiLight = new THREE.HemisphereLight( 0xffffff, 0xffffff, 0.6 );
+				hemiLight.color.setHSL( 0.6, 1, 0.6 );
+				hemiLight.groundColor.setHSL( 0.095, 1, 0.75 );
+				hemiLight.position.set( 0, 500, 0 );
+				scene.add( hemiLight );
+
+
+				var dirLight = new THREE.DirectionalLight(0xffffff, 1);
+				dirLight.position.set(10, 10, 10);
+				dirLight.castShadow = true;
+				scene.add(dirLight);
+
+				
 				backgroundPlane.position.z = -50;
 				scene.add(backgroundPlane);
 				
-				// Pot
-				material =
-						new THREE.LineBasicMaterial( { color: 0x000000, linewidth: 2 } );
-		
 				camera = new THREE.PerspectiveCamera(50,
 																						 width/height, 0.1, 1000);
 				renderer = new THREE.WebGLRenderer( { antialias: true } );
+				renderer.shadowMap.enabled = true;				
+
+				
+				// Ground
+				var plane = new THREE.Mesh(
+					new THREE.PlaneBufferGeometry( 100, 100 ),
+					new THREE.MeshPhongMaterial( { color: 0x999999, specular: 0x101010 } )
+				);
+				plane.receiveShadow= true;
+				plane.rotation.x = -Math.PI/2;
+				plane.position.y = -15;
+//				scene.add( plane );
 
 				renderer.setSize( width, height );
 				parentElement.appendChild(renderer.domElement);
 
-				camera.position.z = 40;
+				camera.position.z = 60;
 				camera.position.y += 10;
-				//camera.lookAt( scene.position );
+				// camera.lookAt( scene.position );
 				renderer.render(scene, camera);
 		}
 		
 		function render() {
 				requestAnimationFrame( render );
 				renderer.render( scene, camera );
-				if(wireframe && rotateP){
-						wireframe.rotation.z += 0.01;
+				if(pot && rotateP){
+						pot.rotation.z += 0.01;
 				}
 		}
 
